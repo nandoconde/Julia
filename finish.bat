515410797fe5009241dc2fb324393eabe418efd6
nandoconde <fernando.conde-pumpido.velasco@alumnos.upm.es>
2019-12-11 10:52:05 +0100

    Added script files for synchronization

---
+++ finish.bat
@@ -0,0 +1,44 @@
+:: This batchfile shuts down the environment after a day's work
+:: Header
+@echo off
+echo.
+echo -----------------------------------------------
+echo                SYNC JULIA
+echo -----------------------------------------------
+echo.
+
+:: Wait until files are saved
+:savePoint
+echo Save all files (Ctrl + S)
+echo Are all files saved?
+echo [Y/y = yes] 
+set /p savedFiles= ""
+if %savedFiles%==y goto :sync1
+if %savedFiles%==Y goto :sync1
+goto :savePoint
+
+
+:: Julia repo syncing
+:sync1
+echo Do you want to stage, commit and push Julia?
+echo [N/n = no, else yes] 
+set /p Julie= ""
+if %Julie%==n goto :salidaA
+if %Julie%==N goto :salidaA
+echo Enter commit message for Julia:
+set /p Julie= ""
+set Julie="%Julie%"
+git stage -A
+git commit -v -a -m %Julie%
+git push
+echo.
+exit 0
+
+:: No synchronization
+:salidaA
+echo "Aborting Julia syncing"
+echo.
+echo -----------------------------------------------
+echo -----------------------------------------------
+echo.
+exit 0
---
+++ startup.bat
@@ -0,0 +1,13 @@
+:: This batchfile starts up the environment for a day's work
+:: Load Julia
+@echo off
+echo.
+echo -----------------------------------------------
+echo                STARTUP JULIA
+echo -----------------------------------------------
+echo.
+git pull
+echo.
+echo -----------------------------------------------
+echo -----------------------------------------------
+echo.
